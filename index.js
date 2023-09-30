9d35eca10d3ef1550878bc6a18ed7cc9d9045ecb
Kauã Nascimento <kauasilva2861>
2023-09-30 13:50:18 -0300

    primeiro trabalho JS

---
+++ index.js
@@ -0,0 +1,17 @@
+/*let a = Number (prompt("Insira um valor: ")) 
+let b = Number (prompt("Insira outro valor: "))
+alert(a+b)*/
+
+let nome = (prompt("Digite seu nome: "))
+let idade = Number(prompt("Digite seu ano de nascimento: "))
+
+alert(`Seu nome: ${nome}`)
+alert(`Sua idade: ${2023-idade}`)
+if(idade > 17){
+    alert("voce é maior de idade")
+    }
+
+else{
+"você não é maior de idade"
+}
+alert(`Em 2050 você tera: ${2050-idade}`)
